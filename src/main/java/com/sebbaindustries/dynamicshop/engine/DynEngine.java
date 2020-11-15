f8c9a91d2df2b7b38cb9ed4f7ffbbd467a7186ae
SebbaIndustries <sebbaindustries@gmail.com>
2020-11-15 21:45:39 +0100

    Ported messages to json file format

--- src/main/java/com/sebbaindustries/dynamicshop/engine/DynEngine.java
+++ src/main/java/com/sebbaindustries/dynamicshop/engine/DynEngine.java
@@ -3,6 +3,7 @@ package com.sebbaindustries.dynamicshop.engine;
 import com.sebbaindustries.dynamicshop.Core;
 import com.sebbaindustries.dynamicshop.engine.components.ShopCategory;
 import com.sebbaindustries.dynamicshop.messages.Message;
+import com.sebbaindustries.dynamicshop.utils.Color;
 import com.sebbaindustries.dynamicshop.utils.ObjectUtils;
 
 import java.util.ArrayList;
@@ -20,9 +21,7 @@ public class DynEngine {
 
     public void initialize() {
         Core.gCore().fileManager.generateCatDirs(core);
-        ObjectUtils.saveGsonFile("messages", new Message());
-        Core.gCore().message = ObjectUtils.getGsonFile("messages", Message.class);
 
-        System.out.println(Message.get().noPermission);
+        Core.gCore().message = ObjectUtils.getGsonFile("messages", Message.class);
     }
 }
--- src/main/java/com/sebbaindustries/dynamicshop/messages/Message.java
+++ src/main/java/com/sebbaindustries/dynamicshop/messages/Message.java
@@ -1,14 +1,13 @@
 package com.sebbaindustries.dynamicshop.messages;
 
 import com.sebbaindustries.dynamicshop.Core;
-import com.sebbaindustries.dynamicshop.utils.Color;
 
 /**
  * @author SebbaIndustries
  */
 public class Message {
 
-    public String noPermission = Color.format("&cYou lack security clearance! This incident will be reported to the general.");
+    public String noPermission = "&cYou lack security clearance! This incident will be reported to the general.";
 
     public static Message get() {
         return Core.gCore().message;
