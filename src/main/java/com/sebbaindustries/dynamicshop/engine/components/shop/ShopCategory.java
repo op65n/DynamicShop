53d4ddefa11b961d920e078cb014287b8639ccc3
SebbaIndustries <sebbaindustries@gmail.com>
2020-12-05 17:45:08 +0100

    ShopCategory implementation, not implemented yet

---
+++ src/main/java/com/sebbaindustries/dynamicshop/engine/components/shop/ShopCategory.java
@@ -0,0 +1,4 @@
+package com.sebbaindustries.dynamicshop.engine.components.shop;
+
+public interface ShopCategory extends com.sebbaindustries.dynamicshop.engine.structure.Serializable {
+}
---
+++ src/main/java/com/sebbaindustries/dynamicshop/engine/components/shop/implementations/ShopCategoryImpl.java
@@ -0,0 +1,13 @@
+package com.sebbaindustries.dynamicshop.engine.components.shop.implementations;
+
+import com.sebbaindustries.dynamicshop.engine.components.shop.ShopCategory;
+
+public class ShopCategoryImpl implements ShopCategory {
+
+
+    @Override
+    public void serialize() {
+
+    }
+
+}
