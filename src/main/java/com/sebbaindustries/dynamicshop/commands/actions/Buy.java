1d592e2e8bafe23d6237d81c94e32de5f875dba5
SebbaIndustries <sebbaindustries@gmail.com>
2020-12-05 22:07:25 +0100

    Multimap should provide cleaner config file.

--- src/main/java/com/sebbaindustries/dynamicshop/commands/actions/Buy.java
+++ src/main/java/com/sebbaindustries/dynamicshop/commands/actions/Buy.java
@@ -6,6 +6,7 @@ import com.sebbaindustries.dynamicshop.commands.components.CommandFactory;
 import com.sebbaindustries.dynamicshop.commands.components.ICmd;
 import com.sebbaindustries.dynamicshop.commands.components.ITab;
 import com.sebbaindustries.dynamicshop.engine.DynEngine;
+import com.sebbaindustries.dynamicshop.utils.ObjectUtils;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
@@ -30,8 +31,12 @@ public class Buy extends CommandFactory implements ICmd, ITab {
     public void execute(@NotNull CommandSender sender, @NotNull String[] args) {
         TomlWriter writer = new TomlWriter.Builder()
                 .build();
+
+        System.out.println(ObjectUtils.deserializeObjectToString(DynEngine.items));
+        System.out.println(" ");
+        System.out.println(ObjectUtils.deserializeObjectToString(DynEngine.items.get("Item")));
         try {
-            writer.write(DynEngine.items, new File(Core.gCore().core.getDataFolder() + "/" + "temp" + ".toml"));
+            writer.write(DynEngine.items.get("Item"), new File(Core.gCore().core.getDataFolder() + "/" + "temp" + ".toml"));
         } catch (IOException e) {
             e.printStackTrace();
         }
