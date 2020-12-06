e10af0e1ae418f42bb06aa0958a9a8dc49adcb0e
SebbaIndustries <sebbaindustries@gmail.com>
2020-12-06 13:24:51 +0100

    New category util

--- src/main/java/com/sebbaindustries/dynamicshop/utils/FileUtils.java
+++ src/main/java/com/sebbaindustries/dynamicshop/utils/FileUtils.java
@@ -1,8 +1,12 @@
 package com.sebbaindustries.dynamicshop.utils;
 
 import com.sebbaindustries.dynamicshop.Core;
+import com.sebbaindustries.dynamicshop.engine.structure.DirectoryStructure;
+import com.sebbaindustries.dynamicshop.log.PluginLogger;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 
 @SuppressWarnings("unused")
 public final class FileUtils {
@@ -10,6 +14,15 @@ public final class FileUtils {
     public static boolean fileExists(String fileName) {
         File f = new File(Core.gCore().core.getDataFolder() + "/" + fileName);
         return f.isFile() && !f.isDirectory();
+    }
+
+    public static List<String> getFilePathsInCategories() {
+        File categoryDirectory = new File(DirectoryStructure.Directory.CATEGORIES.path);
+        if (!categoryDirectory.isDirectory()) {
+            PluginLogger.logSevere("Can't find " + DirectoryStructure.Directory.CATEGORIES.path + " directory!");
+            return null;
+        }
+        return Arrays.asList(categoryDirectory.list());
     }
 
 }
