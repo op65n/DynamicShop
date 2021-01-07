e52e64cdb6028a559d25aff23a4f2bd4356d5a8f
SebbaIndustries <sebbaindustries@gmail.com>
2021-01-07 11:51:37 +0100

    Engine now reloads on startup

--- src/main/java/com/sebbaindustries/dynamicshop/global/ServerPlugin.java
+++ src/main/java/com/sebbaindustries/dynamicshop/global/ServerPlugin.java
@@ -62,6 +62,8 @@ public class ServerPlugin {
         );
         Core.engineLogger.log(Color.format("Color: &r■&0■&1■&2■&3■&4■&5■&6■&7■&8■&9■&a■&b■&c■&d■&e■&f■"));
         Core.pluginLogger.log("=============================================");
+
+        Core.gCore().getEngine().reload();
     }
 
     /**
