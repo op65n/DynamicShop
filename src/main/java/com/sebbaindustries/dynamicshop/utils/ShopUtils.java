bb977e579531519e0189b39f13cfdbc198a2cdd1
SebbaIndustries <sebbaindustries@gmail.com>
2021-01-09 22:20:52 +0100

    New Shop components

---
+++ src/main/java/com/sebbaindustries/dynamicshop/utils/ShopUtils.java
@@ -0,0 +1,14 @@
+package com.sebbaindustries.dynamicshop.utils;
+
+import com.sebbaindustries.dynamicshop.engine.components.EItemType;
+import org.apache.commons.lang3.tuple.Pair;
+
+public class ShopUtils {
+
+    public static Pair<EItemType, String> getTypeAndItemPair(String material, String texture, String base64) {
+        if (base64 != null) return Pair.of(EItemType.BASE64, base64);
+        if (texture != null) return Pair.of(EItemType.TEXTURE, texture);
+        if (material != null) return Pair.of(EItemType.MATERIAL, material.toUpperCase());
+        return Pair.of(EItemType.MATERIAL, "ACACIA_BOAT");
+    }
+}
