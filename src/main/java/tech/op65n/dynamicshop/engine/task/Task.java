7676e6e56da0d7295e6c281b0904b010e78031c8
SebbaIndustries <sebbaindustries@gmail.com>
2021-01-12 00:42:58 +0100

    Future thread stuff

--- src/main/java/tech/op65n/dynamicshop/engine/task/Task.java
+++ src/main/java/tech/op65n/dynamicshop/engine/task/Task.java
@@ -1,8 +1,25 @@
 package tech.op65n.dynamicshop.engine.task;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Task {
+
+    public static ExecutorService executorService = Executors.newFixedThreadPool(30);
+
+    public static Future<Boolean> asyncFuture(Runnable runnable) {
+        return executorService.submit(() -> {
+            try {
+                runnable.run();
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
+        });
+    }
 
     public static void async(Runnable runnable) {
         CompletableFuture.runAsync(runnable).exceptionally(throwable -> {
