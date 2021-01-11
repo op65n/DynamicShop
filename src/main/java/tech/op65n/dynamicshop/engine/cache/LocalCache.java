c08bc30b526483f68fa158559479980295091a06
SebbaIndustries <sebbaindustries@gmail.com>
2021-01-12 00:42:10 +0100

    Yea this will change

--- src/main/java/tech/op65n/dynamicshop/engine/cache/LocalCache.java
+++ src/main/java/tech/op65n/dynamicshop/engine/cache/LocalCache.java
@@ -1,7 +1,10 @@
 package tech.op65n.dynamicshop.engine.cache;
 
+import com.google.common.collect.LinkedListMultimap;
+import com.google.common.collect.Multimap;
+import org.apache.commons.lang3.tuple.Pair;
 import tech.op65n.dynamicshop.Core;
-import tech.op65n.dynamicshop.database.DBSetup;
+import tech.op65n.dynamicshop.engine.structure.ItemStruct;
 import tech.op65n.dynamicshop.engine.structure.ShopCategoryStruct;
 import tech.op65n.dynamicshop.utils.FileUtils;
 import tech.op65n.dynamicshop.utils.ObjectUtils;
@@ -55,6 +58,8 @@ public class LocalCache {
         Collection<ShopCategoryStruct> differentCached = new HashSet<>(loaded);
         Collection<ShopCategoryStruct> differentLoaded = new HashSet<>(cached);
 
+        Multimap<String, Pair<ItemStruct, ItemStruct>> changedItems = LinkedListMultimap.create();
+
         cached.forEach(cachedEntry -> loaded.forEach(loadedEntry -> {
             if (loadedEntry.getFilename().equals(cachedEntry.getFilename())) {
                 differentCached.remove(loadedEntry);
@@ -62,16 +67,78 @@ public class LocalCache {
             }
         }));
 
+        /*
+        cachedEntry.getItem().forEach(cachedItemStruct -> loadedEntry.getItem().forEach(loadedItemStruct -> {
+                    ItemStruct newItemStruct = applyDifferences(cachedItemStruct, loadedItemStruct);
+                    if (newItemStruct != null) changedItems.put(loadedEntry.getFilename(), Pair.of(cachedItemStruct, newItemStruct));
+                }));
+         */
 
-        Core.devLogger.log("Different cached");
-        differentCached.forEach(category -> Core.devLogger.log(category.getFilename()));
-
-
-        Core.devLogger.logWarn("Different loaded");
-        differentLoaded.forEach(loadedCategoryName -> Core.devLogger.log(loadedCategoryName.getFilename()));
+        Core.gCore().setShopCache(new ShopCache());
+        Core.gCore().getShopCache().init(new ArrayList<>(differentCached), new ArrayList<>(differentLoaded));
 
         categoryFileInfo.setCategories(new ArrayList<>(Core.gCore().getEngine().container().getPrioritizedCategoryList()));
 
+    }
+
+    private ItemStruct applyDifferences(ItemStruct cItem, ItemStruct lItem) {
+        boolean isChanged = false;
+        ItemStruct itm = cItem;
+
+        // Material
+        if (!Objects.equals(cItem.getMaterial(), lItem.getMaterial())) {
+            itm.setMaterial(lItem.getMaterial());
+            isChanged = true;
+        }
+        // Texture
+        if (!Objects.equals(cItem.getTexture(), lItem.getTexture())) {
+            itm.setTexture(lItem.getTexture());
+            isChanged = true;
+        }
+
+        // Base64
+        if (!Objects.equals(cItem.getBase64(), lItem.getBase64())) {
+            itm.setBase64(lItem.getBase64());
+            isChanged = true;
+        }
+
+        // Priority
+        if (!Objects.equals(cItem.getPriority(), lItem.getPriority())) {
+            itm.setPriority(lItem.getPriority());
+            isChanged = true;
+        }
+
+        // Display
+        if (!Objects.equals(cItem.getDisplay(), lItem.getDisplay())) {
+            itm.setDisplay(lItem.getDisplay());
+            isChanged = true;
+        }
+
+        // Lore
+        if (!Objects.equals(cItem.getLore(), lItem.getLore())) {
+            itm.setLore(lItem.getLore());
+            isChanged = true;
+        }
+
+        // Command
+        if (!Objects.equals(cItem.getCommand(), lItem.getCommand())) {
+            itm.setCommand(lItem.getCommand());
+            isChanged = true;
+        }
+
+        // Price buy
+        if (!Objects.equals(cItem.getPrice_buy(), lItem.getPrice_buy())) {
+            itm.setPrice_buy(lItem.getPrice_buy());
+            isChanged = true;
+        }
+
+        // Price sell
+        if (!Objects.equals(cItem.getPrice_sell(), lItem.getPrice_sell())) {
+            itm.setPrice_sell(lItem.getPrice_sell());
+            isChanged = true;
+        }
+
+        return isChanged ? itm : null;
     }
 
 }
