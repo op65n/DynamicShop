6e2d5073ffd4cfb2a220bfdef281c02025f1476a
SebbaIndustries <sebbaindustries@gmail.com>
2021-01-14 23:36:20 +0100

    Category can now be constructed from struct

--- src/main/java/tech/op65n/dynamicshop/engine/components/SCategory.java
+++ src/main/java/tech/op65n/dynamicshop/engine/components/SCategory.java
@@ -3,25 +3,40 @@ package tech.op65n.dynamicshop.engine.components;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import tech.op65n.dynamicshop.engine.structure.ItemStruct;
+import tech.op65n.dynamicshop.engine.structure.ShopCategoryStruct;
+import tech.op65n.dynamicshop.utils.ShopUtils;
 
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
 @Getter
 @Setter
-@NoArgsConstructor
 public class SCategory {
 
-    private int ID;
+    private Integer ID;
 
-    private int priority;
+    private Integer priority;
 
     private String filename;
 
     private String name;
 
-    private SIcon icon = new SIcon();
+    private SIcon icon;
 
     private Map<Integer, SItem> items = new TreeMap<>();
+
+    public SCategory(ShopCategoryStruct struct) {
+        this.priority = struct.getPriority();
+        this.filename = struct.getFilename();
+        this.name = struct.getName();
+        this.icon = new SIcon(struct.getIcon());
+        int cnt = 0;
+        for (ItemStruct iStruct : struct.getItem()) {
+            items.put(cnt, new SItem(iStruct, struct.getFilename()));
+            cnt++;
+        }
+    }
 
 }
