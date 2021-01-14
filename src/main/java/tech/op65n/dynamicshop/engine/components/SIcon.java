d6a489a787a7f606a17597b386a594123fdfde2f
SebbaIndustries <sebbaindustries@gmail.com>
2021-01-14 23:35:36 +0100

    Added constructor from item stack.

--- src/main/java/tech/op65n/dynamicshop/engine/components/SIcon.java
+++ src/main/java/tech/op65n/dynamicshop/engine/components/SIcon.java
@@ -3,6 +3,8 @@ package tech.op65n.dynamicshop.engine.components;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import tech.op65n.dynamicshop.engine.structure.IconStruct;
+import tech.op65n.dynamicshop.utils.ShopUtils;
 
 import java.util.List;
 
@@ -18,5 +20,13 @@ public class SIcon {
     private String display;
 
     private List<String> lore;
+
+    public SIcon(IconStruct struct) {
+        var pair = ShopUtils.getTypeAndItemPair(struct.getMaterial(), struct.getTexture(), struct.getBase64());
+        this.type = pair.getLeft();
+        this.item = pair.getRight();
+        this.display = struct.getDisplay();
+        this.lore = struct.getLore();
+    }
 
 }
