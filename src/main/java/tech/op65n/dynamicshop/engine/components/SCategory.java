7927350ab13695e4484130b7cb7532dec11456e6
SebbaIndustries <sebbaindustries@gmail.com>
2021-01-15 23:46:51 +0100

    Modification for struct constructor

--- src/main/java/tech/op65n/dynamicshop/engine/components/SCategory.java
+++ src/main/java/tech/op65n/dynamicshop/engine/components/SCategory.java
@@ -1,23 +1,19 @@
 package tech.op65n.dynamicshop.engine.components;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import tech.op65n.dynamicshop.engine.structure.ItemStruct;
 import tech.op65n.dynamicshop.engine.structure.ShopCategoryStruct;
-import tech.op65n.dynamicshop.utils.ShopUtils;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 @Getter
 @Setter
 public class SCategory {
 
-    private Integer ID;
+    private int ID = -1;
 
-    private Integer priority;
+    private int priority = 1000;
 
     private String filename;
 
@@ -25,18 +21,14 @@ public class SCategory {
 
     private SIcon icon;
 
-    private Map<Integer, SItem> items = new TreeMap<>();
+    private List<SItem> items = new ArrayList<>();
 
     public SCategory(ShopCategoryStruct struct) {
         this.priority = struct.getPriority();
         this.filename = struct.getFilename();
         this.name = struct.getName();
         this.icon = new SIcon(struct.getIcon());
-        int cnt = 0;
-        for (ItemStruct iStruct : struct.getItem()) {
-            items.put(cnt, new SItem(iStruct, struct.getFilename()));
-            cnt++;
-        }
+        struct.getItem().forEach(struct1 -> items.add(new SItem(struct1, struct.getFilename())));
     }
 
 }
