a86967115e9518e816c000d1dc7368506ad09553
SebbaIndustries <sebbaindustries@gmail.com>
2021-01-15 23:47:51 +0100

    This is here just for build spec

---
+++ src/main/java/tech/op65n/dynamicshop/engine/container/Pricing.java
@@ -0,0 +1,18 @@
+package tech.op65n.dynamicshop.engine.container;
+
+public class Pricing {
+
+    private double priceBuy;
+    private double priceSell;
+
+    private int buys;
+    private int sells;
+
+    private int stock;
+
+    private double calculatePriceBuy() {
+        return 0.00;
+    }
+
+
+}
